9afbf2a1e51d2991db4e2250a9f836ed6a727408
Denys Haidemskyi <122185695+Haidemskyi@users.noreply.github.com>
2026-04-16 21:41:03 -0500

    refactor: remove unused styling files and cleanup project structure

--- web/src/app/(dashboard)/recruiting/team/page.tsx
+++
@@ -1,7 +0,0 @@
-import { getUsers } from "@/app/actions/users";
-import TeamPage from "./client";
-
-export default async function Page() {
-    const users = await getUsers();
-    return <TeamPage users={users} />;
-}
